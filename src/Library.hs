4ac862538704a5f1f789edf4ca6451e8e4a9e6e0
matias-escordamaglia <mescordamaglia@frba.utn.edu.ar>
2023-05-14 12:37:55 -0300

    Agrego descripcion a Tests + Punto 2 de

--- src/Library.hs
+++ src/Library.hs
@@ -15,6 +15,7 @@ data Persona = Persona {
 type ChicaSuperPoderosa = Persona
 
 data Amenaza = Amenaza{
+    nombreA :: String,
     proposito :: String,
     nivelDePoder :: Number,
     debilidades :: [String]
@@ -70,16 +71,19 @@ silico = Persona {
 -- ====================================================================== --
 
 mojojojo = Amenaza{
+    nombreA = "Mojo Jojo",
     proposito = "Destruir a las Chicas Superpoderosas",
     nivelDePoder =70,
     debilidades =["Velocidad","Superfuerza"]
 }
 princesa = Amenaza{
+    nombreA = "Princesa",
     proposito = "Ser la unica Chica Superpoderosa",
     nivelDePoder =95,
     debilidades =["Burbujas","Golpes fuertes"]
 }
 bandaGangrena = Amenaza{
+    nombreA = "Banda Gangrena",
     proposito = "Esparcir el caos y Hacer que todos sean flojos y peleen entre ellos",
     nivelDePoder =49,
     debilidades =["Escuchar canciones de Luciano Pereyra","Superfuerza", "Kryptonita"]
@@ -113,18 +117,16 @@ Si una amenaza tiene un daño potencial mayor al doble del número de
 habitantes de la ciudad, entonces puede atacar a la ciudad. -}
 
 amenazaPuedeAtacarCiudad :: Ciudad -> Amenaza -> Bool
-amenazaPuedeAtacarCiudad ciudad = ($(*2).cantidadDeHabitantes $ciudad).(>).danioPotencialAmenaza
+amenazaPuedeAtacarCiudad ciudad = ((<).(*2).cantidadDeHabitantes $ciudad).danioPotencialAmenaza
 
 -- ====================================================================== --
 --                 Integrante 3
 -- ====================================================================== --
-{-
-    Saber si una chica puede vencer a una amenaza. 
+{- Saber si una chica puede vencer a una amenaza. 
     Si tiene longitud de propósito par, ocurre si la resistencia es mayor 
     a la mitad del daño potencial de una amenaza. 
     Si el propósito tiene longitud impar entonces, es suficiente que 
-    la resistencia sea mayor al daño potencial.    
--}
+    la resistencia sea mayor al daño potencial.    -}
 
 propositoEsPar :: Amenaza -> Bool
 propositoEsPar = even.length.proposito
@@ -144,13 +146,8 @@ puedeVencerAmenaza chicasuperpoderosa amenaza
     | otherwise = False
 
 -- ====================================================================== --
---                 Integrante 4
+--                 Integrante 4 
 -- ====================================================================== --
-{- Determinar si una amenaza es de nivel alto, lo que ocurre si tiene 
-    una cantidad par de debilidades, las mismas no incluyen kryptonita 
-    y su daño potencial es mayor a 50.-}
-
-
 tieneKryptonita = elem "Kriptonita"
 
 amenazaDeNivelAlto :: Amenaza -> Bool
@@ -158,3 +155,63 @@ amenazaDeNivelAlto amenaza = operacionDeDebilidades even amenaza
                     && (not.tieneKryptonita.debilidades) amenaza
                     && ((>50).danioPotencialAmenaza) amenaza
 
+-- ====================================================================== --
+--                 Integrante 1- Entrada en calor
+-- ====================================================================== --
+ {-Determinar si una amenaza es invulnerable para una chica, lo cual ocurre 
+ si la chica no tiene habilidades que sean debilidad de la amenaza-}
+
+-- ====================================================================== --
+--                 Integrante 2 - Entrada en calor
+-- ====================================================================== --
+{-Dada una lista de amenazas, determinar cuál es la amenaza preponderante, 
+que es aquella que tenga más nivel de poder.-}
+
+
+--amenazaPreponderante lista = (maximum.map nivelDePoder) lista
+amenazaPreponderante lista = unwords.map nombreA.filter ((==(maximum.map nivelDePoder) lista).nivelDePoder) $lista
+-- ====================================================================== --
+--                 Integrante 3 - Entrada en calor
+-- ====================================================================== --
+{-Dada una lista de chicas y una amenaza, determinar los nombres de aquellas 
+que pueden vencer la amenaza.-}
+
+-- ====================================================================== --
+--                 Integrante 4 - Entrada en calor
+-- ====================================================================== --
+{-En base a una lista de chicas, saber cuáles tienen nombre que empieza 
+con B y más de una habilidad.-}
+
+-- ====================================================================== --
+--                 Integrante 1- Yendo al nutricionista
+-- ====================================================================== --
+ {- + La sustancia X la deja sin resistencia.
+    + La cerveza siempre se toma con amigos, aunque sean potenciales, así 
+    para esta bebida se conoce las personas con las que se está tomando, y 
+    las que no sean amigas, se agregan como tales.-}
+
+-- ====================================================================== --
+--                 Integrante 2 - Yendo al nutricionista
+-- ====================================================================== --
+{- + El saborizador tiene diferentes gustos, como "Fresa", "Arándano" y "Cereza", 
+entre otros. Un saborizador reduce la resistencia de la Chica que lo consume 
+tanto como la cantidad de letras que tenga el sabor. Por ejemplo, si alguien 
+toma saborizador de Fresa, su resistencia se reduciría en 5 unidades.
++ El querido ferne’, donde al consumirlo obtiene automáticamente la habilidad 
+de “Chef de Asados”. Si ya la tiene, no se agrega.-}
+
+-- ====================================================================== --
+--                 Integrante 3 - Yendo al nutricionista
+-- ====================================================================== --
+{- + El Gatorei, cuyo consumo está recomendado por el doctor Bilardo, es una bebida que
+recupera 5 puntos de resistencia a la Chica que lo consume por cada amiga que tenga.
++ El vodka afecta seriamente a una Chica Superpoderosa, haciendo que cambie su nombre 
+perdiendo la última letra del mismo por cada shot que tome. Si toma más shots que los 
+que su nombre acepta según su longitud, simplemente queda con nombre vacío, no hace falta nada especial.-}
+
+-- ====================================================================== --
+--                 Integrante 4 - Yendo al nutricionista
+-- ====================================================================== --
+{- + El caramelo líquido reduce 10 unidades de resistencia a la Chica que lo consume.
++ La cocucha (que sabemos que saca hasta el óxido), le elimina la primera habilidad 
+a quien la ingiera. Si no tiene habilidades, no causa ningún efecto.-}
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,24 +5,35 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test Integrante 1" $ do
-    it "Punto de Integrante 1 funciona correctamente" $ do
-      danioPotencialAmenaza mojojojo `shouldBe` 64   
-      danioPotencialAmenaza princesa `shouldBe` 89 
-      danioPotencialAmenaza bandaGangrena `shouldBe` 40   
-  describe "Test Integrante 2" $ do
-    it "Punto de Integrante 2 funciona correctamente" $ do
-       bandaGangrena `shouldNotSatisfy` amenazaPuedeAtacarCiudad saltadilla
-       mojojojo `shouldSatisfy` amenazaPuedeAtacarCiudad saltadilla
-       princesa `shouldSatisfy` amenazaPuedeAtacarCiudad saltadilla
-  describe "Test Integrante 3" $ do
-    it "Punto de Integrante 3 funciona correctamente" $ do
-       princesa `shouldSatisfy` propositoEsPar
-       princesa `shouldNotSatisfy` puedeVencerAmenaza burbuja
-       mojojojo `shouldSatisfy` propositoEsPar
-       mojojojo `shouldSatisfy` puedeVencerAmenaza bombon
-  describe "Test Integrante 4" $ do
-    it "Punto de Integrante 4 funciona correctamente" $ do
-       princesa `shouldSatisfy` amenazaDeNivelAlto 
-       bandaGangrena `shouldNotSatisfy` amenazaDeNivelAlto
+  describe "Tests de la primera entrega" $ do
+    context "Integrante 1: danioPotencialAmenaza/1" $ do
+      it "El danio potencial de mojojojo da 64" $ do
+        danioPotencialAmenaza mojojojo `shouldBe` 64   
+      it "El danio potencial de princesa da 89" $ do
+        danioPotencialAmenaza princesa `shouldBe` 89 
+      it "El danio potencial de bandaGangrena da 40" $ do
+        danioPotencialAmenaza bandaGangrena `shouldBe` 40   
+    context "Integrante 2: amenazaPuedeAtacarCiudad/2" $ do
+      it "bandaGangrena no puede atacar saltadilla" $ do
+        bandaGangrena `shouldNotSatisfy` amenazaPuedeAtacarCiudad saltadilla
+      it "mojojojo puede atacar saltadilla" $ do
+        mojojojo `shouldSatisfy` amenazaPuedeAtacarCiudad saltadilla
+      it "princesa puede atacar saltadilla" $ do   
+        princesa `shouldSatisfy` amenazaPuedeAtacarCiudad saltadilla
+    context "Integrante 3: propositoEsPar/1 y puedeVencerAmenaza/2" $ do
+      it "El proposito de princesa es par" $ do
+        princesa `shouldSatisfy` propositoEsPar
+      it "Burbuja no puede vencer a princesa" $ do  
+        princesa `shouldNotSatisfy` puedeVencerAmenaza burbuja
+      it "El proposito de mojojojo es par" $ do  
+        mojojojo `shouldSatisfy` propositoEsPar
+      it "bombon puede vencer a mojojojo" $ do  
+        mojojojo `shouldSatisfy` puedeVencerAmenaza bombon
+    context "Integrante 4: amenazaDeNivelAlto/1" $ do
+      it "El nivel de amenaza de princesa es alto" $ do
+        princesa `shouldSatisfy` amenazaDeNivelAlto 
+      it "El nivel de amenaza de bandaGangrena no es alto" $ do  
+        bandaGangrena `shouldNotSatisfy` amenazaDeNivelAlto
        
+--Traten de cambiar la estructura en la que plantean los tests, que no sea un simple "it" que diga "Punto de Integrante 2 funciona correctamente", 
+--sino que por cada it se haga un testeo y que la descripción diga lo que realmente esta haciendo
